df416757214c9643223d71e485ad4be93bd112f5
TaaviM88Laptop <juomakuski@hotmail.com>
2018-12-10 11:28:25 +0200

    movementScript

---
+++ Neko Gear Remaster/Assets/Scripts/Dev Scripts/MovementPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    public float forwardSpeed = 1f;
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * forwardSpeed;
+        verticalMove = Input.GetAxis("Vertical") * forwardSpeed;
+
+        transform.position += new Vector3(horizontalMove, verticalMove, 0);
+    }
+}
